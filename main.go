29ba4c5323ebeb5b97edb7d77c7ed55d5e8eee88
mohits-git <152606488+mohits-git@users.noreply.github.com>
2024-12-22 15:36:56 +0530

    feat: add stateful handlers with apiconfig and middleware

--- main.go
+++ main.go
@@ -3,15 +3,52 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+  res := "Hits: " + strconv.Itoa(int(hits))
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(res))
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	port := "8080"
 	filepathRoot := "."
+	apiCfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+	apiCfg.fileserverHits.Store(0)
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(
+		http.StripPrefix(
+			"/app",
+			http.FileServer(http.Dir(filepathRoot)),
+		),
+	))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handleMetrics)
+	mux.HandleFunc("/reset", apiCfg.handleReset)
 
 	server := &http.Server{
 		Addr:    ":" + port,
