619ff4facf8aa43d5879914e5e55d5739665593b
mohits-git <152606488+mohits-git@users.noreply.github.com>
2024-12-22 12:42:18 +0530

    feat: serve files via http

--- main.go
+++ main.go
@@ -8,14 +8,16 @@ import (
 
 func main() {
 	port := "8080"
+	filepathRoot := "."
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	fmt.Println("Server listening on port", port)
+	fmt.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 }
